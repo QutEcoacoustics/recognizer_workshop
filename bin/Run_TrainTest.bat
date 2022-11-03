4175c79f56118406e48262dc70d392217b66c705
peichins <peichins@users.noreply.github.com>
2022-11-03 10:06:28 +1000

    tidied up file structure

---
+++ bin/Run_TrainTest.bat
@@ -0,0 +1,11 @@
+
+
+
+REM C:/ProgramData/Miniconda3/python.exe c:/Work/NeaR/AppTrainRavenBinary.py -conf config.ini -data-csv "train.csv" -epochs 15 -batch-size 8 -trained-model "model.pth" -lr 0.0005 -lr-decay 0.98 -wav-dir "./audio/train" -spec-dir "./specs"
+
+
+C:/ProgramData/Miniconda3/python.exe c:/Work/NeaR/AppTrainRavenBinary.py -conf config.ini -data-csv "train.csv" -epochs 15 -batch-size 24 -trained-model "model.pth" -lr 0.0005 -lr-decay 0.98 -wav-dir "./audio/train" -spec-dir "./specs"
+
+
+PAUSE
+
